a802c98bad1af035d6fd51679ba56ab88983aacc
RodrigoCortazarOrtega <rodrigo.cortazar463@gmail.com>
2024-09-30 08:52:34 -0600

    Reinicio de validacion captcha

--- login.php
+++ login.php
@@ -1,14 +1,9 @@
 <?php
-session_start();
 $r1=mt_rand(1,9);
 $r2=mt_rand(1,9);
 $captcha=$r1+$r2;
 $errores="";
-if(!isset($_SESSION["captcha"])){
-$_SESSION['captcha']=$captcha;
-}else{
-    echo "la captcha fue: ".$_SESSION["captcha"];
-}
+
 function camposLlenosPost(){
     foreach($_POST as $llave => $valor){
         if(empty($_POST[$llave])){
@@ -17,17 +12,13 @@ function camposLlenosPost(){
     }
     return true;
 }
-
+    
 
 
 
 
 if($_POST && camposLlenosPost()){
-    if( (int)$_POST['captcha']==$_SESSION['captcha'] ){
-        echo "correcto";
-    }else{
-        $errores.="Capcha erronea";
-    }
+    //Validar credenciales    
 }elseif($_POST && !camposLlenosPost()){
     $errores.="campos vacios";
 }
---
+++ pruebas.php
@@ -0,0 +1,40 @@
+<?php 
+
+$a1=[1,2,4];
+$a2=[];
+$d="JEJEJ";
+$d2;
+
+
+echo "<h2>Pruebas funcion 'isempty'</h2>";
+echo "is set? ";
+if(isset($d2)){
+    echo "Si";
+}else{
+    echo "No";
+}
+
+
+
+?>
+<?php
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="#">
+        <input name="user" placeholder="User" style="background:black;color:white;"></input>
+        
+        <input type="submit">
+    </form>
+</body>
+</html
+
+
+*/
+?>
